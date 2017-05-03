d1de7028080af638237b4f914f02ecaa090655dc
Jennavee <jvelez@ayzenberg.com>
2017-05-02 17:52:51 -0700

    moved stuff around

---
+++ Praeses_PoC/Assets/Asset Depot/Scripts/Input/clickMotion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickMotion : MonoBehaviour
+{
+    Vector3 startPos;
+    bool movingF;
+    bool movingB;
+    public float moveSpeed;
+    public float moveDist;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingF)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + moveSpeed);
+            if(transform.localPosition.z >= startPos.z + moveDist)
+            {
+                movingF = false;
+                movingB = true;
+            }
+        }
+
+        if (movingB)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - moveSpeed);
+            if (transform.localPosition.z <= startPos.z)
+            {
+                transform.localPosition = startPos;
+                movingF = false;
+                movingB = false;
+
+                gameObject.SendMessage("finishClick", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+    }
+
+    public void click()
+    {
+        movingF = true;
+    }
+
+
+}
---
+++ Praeses_PoC/Assets/Asset Depot/Scripts/UI/slideOut.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slideOut : MonoBehaviour {
+
+    public float slideSpeed;
+    public float slideDist;
+    bool isMovingF;
+    bool isMovingB;
+    Vector3 startPos;
+    public float timer;
+    bool keptOut;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isMovingF)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x + slideSpeed, startPos.y, startPos.z);
+            if (transform.localPosition.x <= startPos.x + slideDist)
+            {
+                transform.localPosition = new Vector3((startPos.x + slideDist), startPos.y, startPos.z);
+                isMovingF = false;
+            }
+        }
+
+
+        if (isMovingB)
+        {
+
+                transform.localPosition = new Vector3(transform.localPosition.x - slideSpeed, startPos.y, startPos.z);
+                if (transform.localPosition.x >= startPos.x)
+                {
+                    transform.localPosition = startPos;
+                    isMovingF = false;
+                }
+            
+
+
+        }
+
+    }
+
+    public void slideForward()
+    {
+        isMovingF = true;
+        isMovingB = false;
+    }
+
+    public void slideBackward()
+    {
+    if (!keptOut)
+    {
+        isMovingB = true;
+        isMovingF = false;
+    }
+
+    }
+
+    public void slideBackTimer()
+    {
+        Invoke("slideBackward", timer);
+        keptOut = false;
+    }
+
+    public void keepOut()
+    {
+        keptOut = true;
+    }
+}
